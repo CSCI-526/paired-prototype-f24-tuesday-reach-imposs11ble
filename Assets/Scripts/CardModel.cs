f3deb0644bdbe7ef5a62b6e5635edc83d43b78db
Chen Ling Chuang <cchuang2@usc.edu>
2024-09-21 05:19:53 -0700

    feat(playing cards):shuffle cards and display

--- Assets/Scripts/CardModel.cs
+++ Assets/Scripts/CardModel.cs
@@ -5,15 +5,19 @@ using UnityEngine.UI;
 
 public class CardModel : MonoBehaviour
 {
-    public Sprite[] faces;
+    public Sprite[] cardsStack;
     public Image currCard;
-
-    public int cardIndex;
+    private List<int> cardsOrder = new List<int>();
+    public int cursorInCardOrder = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        // not showing the current card
         currCard.enabled = false;
+
+        createCardOrder();
+        ShuffleCards();
     }
 
     // Update is called once per frame
@@ -22,10 +26,34 @@ public class CardModel : MonoBehaviour
         
     }
 
-    public void showCurrCard() {
+    private void createCardOrder() {
+        //create a list of numbers from 0 to 54
+        //playing cards has 52 with 2 ghost card
+        for(int i=0; i< cardsStack.Length; i++)
+        {
+            cardsOrder.Add(i);
+        }
+    }
+
+    private void ShuffleCards() {
+        int numOfCardsleft = cardsOrder.Count;
+        while (numOfCardsleft > 1)
+        {
+            numOfCardsleft --;
+            int k = Random.Range(0,numOfCardsleft);;
+            int value = cardsOrder[k];
+            cardsOrder[k] = cardsOrder[numOfCardsleft];
+            cardsOrder[numOfCardsleft] = value;
+        }
+        Debug.Log("Cards Order: " + string.Join(", ", cardsOrder));
+    }
+
+    public void ShowCurrCard() {
         currCard.enabled = true;
-        int randomNum = Random.Range(0,55);
-        currCard.sprite = faces[randomNum];
+        int indexInStack = cardsOrder[cursorInCardOrder];
+        currCard.sprite = cardsStack[indexInStack];
+        // move cursor to the next card
+        cursorInCardOrder ++;
 
         //let card disappear after 3 secs
         StartCoroutine (HideCardAfterDelay(3));
