e22f9560a12c12c48d30597c768837de831786c1
Chen Ling Chuang <cchuang2@usc.edu>
2024-09-21 02:09:40 -0700

    feat(playing cards):show random card when pressing betting button

---
+++ Assets/Scripts/CardModel.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardModel : MonoBehaviour
+{
+    public Sprite[] faces;
+    public Image currCard;
+
+    public int cardIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currCard.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showCurrCard() {
+        currCard.enabled = true;
+        int randomNum = Random.Range(0,55);
+        currCard.sprite = faces[randomNum];
+    }
+}
