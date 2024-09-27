db0b92cf58174c4c27718c140577f2138bbb3d88
yuhan0309 <156150530+yuhan0309@users.noreply.github.com>
2024-09-26 20:14:59 -0700

    Add point logic

--- Assets/Scripts/CardModel.cs
+++ Assets/Scripts/CardModel.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,9 @@ public class CardModel : MonoBehaviour
     private List<int> cardsOrder = new List<int>();
     public int cursorInCardOrder = 0;
     public float speed = 10.0f;
+    public int score = 0;
+    //public int maxCards = 52;
+    public int extraLives = 0; // Number of lives for Jokers
 
     public Button upbutton;
     public Button equalbutton;
@@ -23,6 +27,7 @@ public class CardModel : MonoBehaviour
 
     public GameObject losePanel;
     public GameObject winPanel;
+    public Text hintText;
 
     // Start is called before the first frame update
     void Start()
@@ -42,18 +47,23 @@ public class CardModel : MonoBehaviour
         ShuffleCards();
 
 
-        // show one card first so user cab bet for the next 
-        int indexInStack = cardsOrder[cursorInCardOrder];
-        currCard.enabled = true;
-        StartCoroutine(HideCardAfter(1));
-        StartCoroutine(BecomePrevCard(indexInStack));
-        cursorInCardOrder++;
-        nextbutton.interactable = true;
+        ShowFirstCard();
 
 
         //initially set win/lose panels inactive
         toggleLosePanel(false);
         toggleWinPanel(false);
+    }
+
+    // Function to show the first card at the start of the game
+    private void ShowFirstCard()
+    {
+        int indexInStack = cardsOrder[cursorInCardOrder];
+        currCard.enabled = true;
+        //StartCoroutine(HideCardAfter(1));
+        StartCoroutine(BecomePrevCard(indexInStack));
+        cursorInCardOrder++;
+        nextbutton.interactable = true;
     }
 
     // Update is called once per frame
@@ -89,73 +99,144 @@ public class CardModel : MonoBehaviour
     public void checkBetResult(string betOperation)
     {
 
-        upbutton.gameObject.SetActive(false);
-        downbutton.gameObject.SetActive(false);
-        equalbutton.gameObject.SetActive(false);
-        spadeButton.gameObject.SetActive(false);
-        clubButton.gameObject.SetActive(false);
-        heartButton.gameObject.SetActive(false);
-        diamondButton.gameObject.SetActive(false);
+        DisableBetButtons();
+
 
         int indexInStack = cardsOrder[cursorInCardOrder];
+        int proInStack = cardsOrder[cursorInCardOrder-1];
         int cardNumber = indexInStack % 13;
         //TODO: notice that cardNumber hasen't take care of 2 joker cards rn,
         // they will be considered as card 1 or 2. need to address this later
-        int prevIndex = cursorInCardOrder - 1;
-        indexInStack = cardsOrder[prevIndex];
-        int prevCardNumber = indexInStack % 13;
+        int cardSuit = indexInStack / 13;
+        int prevCardNumber = proInStack % 13;
+        int prevCardSuit = proInStack / 13;
 
+        int pointsForRound = 1;
         // Debug.Log("cardNumber: " + cardNumber);
         // Debug.Log("prevCardNumber: " + prevCardNumber);
-        if (cardNumber > 10) // it is a J/Q/K or Joker card!
+
+        // Handle Joker cards (assuming 52 and 53 are jokers)
+        if (indexInStack >= 52)
+        {
+            HandleJoker();
+            return;
+        }
+
+
+        if (cardNumber >= 10) // it is a J/Q/K or Joker card!
         {
             //TODO: add features for these types of card when implementing
             // scoring system
+
+            if (cardNumber == 10) 
+            {
+                ProvideColorHint(cardSuit);
+                MoveToNextCardWithoutBet();
+            }
+
+            if (cardNumber == 11) 
+            {
+                ProvideOddEvenHint(cardNumber);
+                MoveToNextCardWithoutBet();
+            }
+
+            if (cardNumber == 12) 
+            {
+                pointsForRound = 2;
+                MoveToNextCardWithoutBet();
+            }
+
+
+
         }
         //TODO: 
 
         if (betOperation.Equals("spade"))
         {
-            if (indexInStack <= 12)
+            if (cardSuit ==0)
             {
+                score += 1* pointsForRound;
                 Debug.Log("Correct!");
             }
             else
             {
-                Debug.Log("False!");
+                if (extraLives > 0) // Check if player has an extra life from a Joker
+                {
+                    extraLives--;
+                    Debug.Log("Incorrect, but you have an extra life! Lives left: " + extraLives);
+                }
+                else
+                {
+                    Debug.Log("Incorrect! Game Over.");
+                    StartCoroutine(ShowGameOver(indexInStack));
+                    return;
+                }
             }
         }
         if (betOperation.Equals("club"))
         {
-            if (13 <= indexInStack && indexInStack <= 26)
+            if (cardSuit == 1)
             {
+                score = score + 1 * pointsForRound;
                 Debug.Log("Correct!");
             }
             else
             {
-                Debug.Log("False!");
+                if (extraLives > 0) // Check if player has an extra life from a Joker
+                {
+                    extraLives--;
+                    Debug.Log("Incorrect, but you have an extra life! Lives left: " + extraLives);
+                }
+                else
+                {
+                    Debug.Log("Incorrect! Game Over.");
+                    StartCoroutine(ShowGameOver(indexInStack));
+                    return;
+                }
             }
         }
         if (betOperation.Equals("heart"))
         {
-            if (40 <= indexInStack && indexInStack <= 52)
+            if (cardSuit == 2)
             {
+                score = score + 1 * pointsForRound;
                 Debug.Log("Correct!");
             }
             else
             {
-                Debug.Log("False!");
+                if (extraLives > 0) // Check if player has an extra life from a Joker
+                {
+                    extraLives--;
+                    Debug.Log("Incorrect, but you have an extra life! Lives left: " + extraLives);
+                }
+                else
+                {
+                    Debug.Log("Incorrect! Game Over.");
+                    StartCoroutine(ShowGameOver(indexInStack));
+                    return;
+                }
             }
         }
         if (betOperation.Equals("diamond"))
         {
-            if (27 <= indexInStack && indexInStack <= 39)
+            if (cardSuit == 3)
             {
+                score = score + 1 * pointsForRound;
                 Debug.Log("Correct!");
             }
             else
             {
-                Debug.Log("False!");
+                if (extraLives > 0) // Check if player has an extra life from a Joker
+                {
+                    extraLives--;
+                    Debug.Log("Incorrect, but you have an extra life! Lives left: " + extraLives);
+                }
+                else
+                {
+                    Debug.Log("Incorrect! Game Over.");
+                    StartCoroutine(ShowGameOver(indexInStack));
+                    return;
+                }
             }
         }
 
@@ -165,42 +246,98 @@ public class CardModel : MonoBehaviour
         {
             if (cardNumber > prevCardNumber)
             {
+                score = score + 1 * pointsForRound;
                 Debug.Log("Correct!");
             }
             else
             {
-                Debug.Log("False!");
+                if (extraLives > 0) // Check if player has an extra life from a Joker
+                {
+                    extraLives--;
+                    Debug.Log("Incorrect, but you have an extra life! Lives left: " + extraLives);
+                }
+                else
+                {
+                    Debug.Log("Incorrect! Game Over.");
+                    StartCoroutine(ShowGameOver(indexInStack));
+                    return;
+                }
             }
         }
-        if (betOperation.Equals("up"))
+        if (betOperation.Equals("equal"))
         {
             if (cardNumber == prevCardNumber)
             {
+                score = score + 3 * pointsForRound;
                 Debug.Log("Correct!");
             }
             else
             {
-                Debug.Log("False!");
+                if (extraLives > 0) // Check if player has an extra life from a Joker
+                {
+                    extraLives--;
+                    Debug.Log("Incorrect, but you have an extra life! Lives left: " + extraLives);
+                }
+                else
+                {
+                    Debug.Log("Incorrect! Game Over.");
+                    StartCoroutine(ShowGameOver(indexInStack));
+                    return;
+                }
             }
         }
         if (betOperation.Equals("down"))
         {
             if (cardNumber < prevCardNumber)
             {
+                score = score + 1 * pointsForRound;
                 Debug.Log("Correct!");
             }
             else
             {
-                Debug.Log("False!");
+                if (extraLives > 0) // Check if player has an extra life from a Joker
+                {
+                    extraLives--;
+                    Debug.Log("Incorrect, but you have an extra life! Lives left: " + extraLives);
+                }
+                else
+                {
+                    Debug.Log("Incorrect! Game Over.");
+                    StartCoroutine(ShowGameOver(indexInStack));
+                    return;
+                }
             }
+        }
+
+
+
+
+
+        cursorInCardOrder++;
+
+        if (score == 11)
+        {
+            toggleWinPanel(true); // Show win if all cards are guessed
+            return;
         }
 
         StartCoroutine(HideCardAfter(1)); // this is to hide the back card image
         StartCoroutine(BecomePrevCard(indexInStack));
-        cursorInCardOrder++;
+   
         // Debug.Log("cursorInCardOrder aaa:" + cursorInCardOrder);
         //return false;
     }
+
+    private void MoveToNextCardWithoutBet()
+    {
+        cursorInCardOrder++;
+
+        int nextCardIndex = cardsOrder[cursorInCardOrder];
+        StartCoroutine(HideCardAfter(1));
+        StartCoroutine(BecomePrevCard(nextCardIndex));
+    }
+
+
     public void ShowCurrCard()
     {
         int indexInStack = cardsOrder[cursorInCardOrder];
@@ -215,19 +352,70 @@ public class CardModel : MonoBehaviour
         Debug.Log("indexInStack: " + indexInStack);
         Debug.Log("cursorInCardOrder: " + cursorInCardOrder);
 
-        // enable 3 bet buttons
-        upbutton.gameObject.SetActive(true);
-        downbutton.gameObject.SetActive(true);
-        equalbutton.gameObject.SetActive(true);
-        spadeButton.gameObject.SetActive(true);
-        clubButton.gameObject.SetActive(true);
-        heartButton.gameObject.SetActive(true);
-        diamondButton.gameObject.SetActive(true);
+        EnableBetButtons();
 
         // move cursor to the next card
         //cursorInCardOrder ++;
 
     }
+
+    private void HandleJoker()
+    {
+        Debug.Log("Joker! ");
+        // For simplicity, assume drawing a joker ends the game
+
+        extraLives++;
+        cursorInCardOrder++;
+
+        StartCoroutine(HideCardAfter(1));
+        StartCoroutine(BecomePrevCard(cardsOrder[cursorInCardOrder]));
+
+
+    }
+
+
+
+    private void ProvideColorHint(int cardSuit)
+    {
+        string colorHint = "";
+        if (cardSuit == 0 || cardSuit == 1) // Spades and Clubs
+        {
+            colorHint = "Black";
+        }
+        else if (cardSuit == 2 || cardSuit == 3) // Hearts and Diamonds
+        {
+            colorHint = "Red";
+        }
+
+        hintText.text = "Hint: The suit color is " + colorHint;
+    }
+
+    private void ProvideOddEvenHint(int cardNumber)
+    {
+        if (cardNumber % 2 == 0) // Even number
+        {
+            hintText.text = "Hint: The card is even.";
+        }
+        else // Odd number
+        {
+            hintText.text = "Hint: The card is odd.";
+        }
+    }
+
+    IEnumerator ShowGameOver(int finalCardIndex)
+    {
+        // Show the final card before displaying "Game Over"
+        currCard.enabled = true;
+        currCard.sprite = cardsStack[finalCardIndex];
+
+        yield return new WaitForSeconds(2);  // Display the final card for 2 seconds
+
+        toggleLosePanel(true);  // Show the Game Over panel after the final card
+    }
+
+
+
+
 
     // show current card that user need to bet
     IEnumerator BecomePrevCard(int preIndex)
@@ -260,5 +448,26 @@ public class CardModel : MonoBehaviour
 
     public void toggleWinPanel(bool isShow) {
         winPanel.SetActive(isShow);
+    }
+    private void DisableBetButtons()
+    {
+        upbutton.gameObject.SetActive(false);
+        downbutton.gameObject.SetActive(false);
+        equalbutton.gameObject.SetActive(false);
+        spadeButton.gameObject.SetActive(false);
+        clubButton.gameObject.SetActive(false);
+        heartButton.gameObject.SetActive(false);
+        diamondButton.gameObject.SetActive(false);
+    }
+
+    private void EnableBetButtons()
+    {
+        upbutton.gameObject.SetActive(true);
+        downbutton.gameObject.SetActive(true);
+        equalbutton.gameObject.SetActive(true);
+        spadeButton.gameObject.SetActive(true);
+        clubButton.gameObject.SetActive(true);
+        heartButton.gameObject.SetActive(true);
+        diamondButton.gameObject.SetActive(true);
     }
 }
